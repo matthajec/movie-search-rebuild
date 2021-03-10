f54fc2b3babe0428b18af561c52e87a46f7d5990
matt hajec <matthew.hajec02@gmail.com>
2021-03-10 06:22:55 -0500

    added lazy loading and ratings to card components

--- src/components/card/MovieCard.tsx
+++ src/components/card/MovieCard.tsx
@@ -1,24 +1,46 @@
-import { Card, CardMedia, CardContent, Typography } from '@material-ui/core'
+import { Card, CardMedia, CardContent, Typography, makeStyles } from '@material-ui/core'
+import Rating from '@material-ui/lab/Rating'
 import { IAPIMovieResult } from '../../interfaces'
+import Lazy from 'react-lazyload'
+
+const useStyles = makeStyles({
+  unratedText: {
+    lineHeight: '24px',
+    fontSize: '18px',
+  }
+})
 
 
-const MovieCard: React.FC<IAPIMovieResult> = ({ title, backdrop_path }) => {
+const MovieCard: React.FC<IAPIMovieResult> = ({ title, backdrop_path, overview, vote_average, vote_count }) => {
+  const classes = useStyles()
+  const isRated = vote_count > 0
+
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        alt={title}
-        height="200"
-        image={`https://image.tmdb.org/t/p/w500${backdrop_path}`}
-        title={title}
-      />
-      <CardContent>
-        <Typography>
-          {title}
-        </Typography>
-      </CardContent>
+    <Lazy>
+      <Card>
+        <CardMedia
+          component="img"
+          alt={title}
+          height="200"
+          image={`https://image.tmdb.org/t/p/w500${backdrop_path}`}
+          title={title}
+        />
+        <CardContent>
+          <Typography component="h3" variant="h6" noWrap>
+            {title}
+          </Typography>
+          {
+            isRated ?
+              <Rating value={vote_average / 2} precision={0.1} readOnly /> :
+              <Typography className={classes.unratedText}>Unrated</Typography>
+          }
+          <Typography noWrap>
+            {overview}
+          </Typography>
+        </CardContent>
 
-    </Card>
+      </Card>
+    </Lazy>
   )
 }
 
